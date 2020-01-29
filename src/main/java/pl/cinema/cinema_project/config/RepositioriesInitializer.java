fee7ae27d952e74b6be6433f4a45964727d76dd9
Khagmar <mateusz.bareja97@gmail.com>
2020-01-29 20:40:45 +0100

    shared:header/footer

--- src/main/java/pl/cinema/cinema_project/config/RepositioriesInitializer.java
+++ src/main/java/pl/cinema/cinema_project/config/RepositioriesInitializer.java
@@ -22,6 +22,18 @@ public class RepositioriesInitializer {
     @Autowired
     private Price_listRepository price_listRepository;
 
+    @Autowired
+    private MoviesRepository moviesRepository;
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Autowired
+    private MovieToRoomRepository movieToRoomRepository;
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
     //@Autowired
     //private PasswordEncoder passwordEncoder;
 
@@ -52,10 +64,53 @@ public class RepositioriesInitializer {
                 }
             }
 
+            Price_list normal = new Price_list("normalny",20);
+            Price_list reduced = new Price_list("ulgowy",16);
             if(price_listRepository.findAll().isEmpty() == true){
-                Price_list normal = price_listRepository.save(new Price_list("normalny",20));
-                Price_list reduced = price_listRepository.save(new Price_list("ulgowy",16));
+                price_listRepository.save(normal);
+                price_listRepository.save(reduced);
             }
+
+            String pom = "Robert Downey Jr. wciela się w jedną z najsłynniejszych postaci światowej literatury w pełnej przygód nowej filmowej wersji opowieści o człowieku, który potrafi rozmawiać ze zwierzętami. Oto doktor Dolittle.\n" +
+                    "\n" +
+                    "Po stracie żony, siedem lat wcześniej, ekscentryczny doktor John Dolittle (Downey Jr.), słynny naukowiec i lekarz weterynarii w wiktoriańskiej Anglii, wiedzie pustelniczy tryb życia, w otoczeniu menażerii swych egzotycznych zwierząt.\n" +
+                    "\n" +
+                    "Kiedy jednak młoda królowa zapada na śmiertelną chorobę, nieustraszony doktor wyrusza w pełną przygód podróż na mityczną wyspę, gdzie spodziewa się znaleźć lekarstwo dla królowej. Wykorzysta w tym swą odwagę i niepodrabialne poczucie humoru, odkryje wiele niezwykłych stworzeń i spotka się ponownie ze starymi wrogami.\n" +
+                    "\n" +
+                    "W wyprawie towarzyszyć mu będzie młody, pewny siebie uczeń (Harry Collett) oraz wesoła ferajna przyjaciół-zwierząt, w tym lękliwy goryl, entuzjastycznie nastawiona do świata kaczka (o, cóż, ptasim móżdżku), niezwykły duet składający się z cynicznego strusia i optymistycznego niedźwiedzia polarnego oraz prawdziwa twardzielka – papuga, najbardziej zaufana przyjaciółka Dolittle’a.\n" +
+                    "\n" +
+                    "Za reżyserię odpowiada nagrodzony Oscarem® Stephen Gaghan („Syriana”, „Traffic”).";
+            Movies movies = new Movies("Doctor Dolittle", "familijny/fantasy/komedia",pom,2020,1,29);
+            if(moviesRepository.findAll().isEmpty() == true)
+            {
+
+                 moviesRepository.save(movies);
+
+            }
+            Room room = new Room(20, 15);
+            if(roomRepository.findAll().isEmpty() == true)
+            {
+
+                roomRepository.save(room);
+
+            }
+            MovieToRoom movieToRoom = new MovieToRoom(room,movies,30,1,2020,20,0);
+            if(movieToRoomRepository.findAll().isEmpty() == true)
+            {
+
+                movieToRoomRepository.save(movieToRoom);
+
+            }
+
+            Reservation reservation = new Reservation(movieToRoom,normal,1,1);
+            if(movieToRoomRepository.findAll().isEmpty() == true)
+            {
+
+                reservationRepository.save(reservation);
+
+            }
+
+
         };
     }
 }
